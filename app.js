a9b7a6434accc5a03e4f0727be262bc4cacfcb01
Wes Reid <bwreid@gmail.com>
2017-04-10 14:31:41 -0700

    Add base movie route
    - Create routes/movies.js file
    - Create new movies/index.hbs view
    - Include routes in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var movies = require('./routes/movies');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/movies', movies);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
