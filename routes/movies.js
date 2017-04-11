9f971b8726bbab9bcc476a047857a628638510ea
Wes Reid <bwreid@gmail.com>
2017-04-10 21:02:44 -0700

    Remove all semicolons :sparkles:

--- routes/movies.js
+++ routes/movies.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var db = require('../db')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   db('movies').select('*').then(movies => {
-    res.render('movies/index', { movies });
+    res.render('movies/index', { movies })
   })
-});
+})
 
-module.exports = router;
+module.exports = router
