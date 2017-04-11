62d35743ad5ab183fe6586b4a822bf8cb290e600
Wes Reid <bwreid@gmail.com>
2017-04-10 17:03:02 -0700

    Add basic query to return all movies

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('movies/index');
+  db('movies').select('*').then(movies => {
+    var movieStr = JSON.stringify(movies)
+    res.render('movies/index', {
+      movies: movieStr
+    });
+  })
 });
 
 module.exports = router;
