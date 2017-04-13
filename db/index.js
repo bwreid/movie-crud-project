9f940507046e6d3d22c6e32b044f28a364f72260
Doubleshot1122 <skibo1122@yahoo.com>
2017-04-13 11:08:08 -0700

    added semicolons to db/index

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
-const env = process.env.DATABASE_URL || 'development'
-const config = require('../knexfile')[env]
-const knex = require('knex')
-const connection = knex(config)
+const env = process.env.DATABASE_URL || 'development';
+const config = require('../knexfile')[env];
+const knex = require('knex');
+const connection = knex(config);
 
 module.exports = connection
