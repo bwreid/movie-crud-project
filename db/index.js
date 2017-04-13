3babf93a212fb140c8a73086a2b404b9a93fd255
Doubleshot1122 <skibo1122@yahoo.com>
2017-04-13 09:48:03 -0700

    updating heroku db settings

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const env = process.env.NODE_ENV || 'development'
+const env = process.env.DATABASE_URL || 'development'
 const config = require('../knexfile')[env]
 const knex = require('knex')
 const connection = knex(config)
