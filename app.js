0af9fe17ff2579138ca685c84a5b95187cd9ad7b
Doubleshot1122 <skibo1122@yahoo.com>
2017-04-13 11:15:22 -0700

    updated db/index env setting

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ hbsUtils.registerPartials(path.join(__dirname, 'views'), {
 })
 
 // uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+// app.use(favicon(path.join(__dirname, 'public','favicon.ico')))
+
 app.use(logger('dev'))
 app.use(methodOverride('_method'))
 app.use(bodyParser.json())
--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const env = process.env.DATABASE_URL || 'development';
+const env = process.env.NODE_ENV || 'development';
 const config = require('../knexfile')[env];
 const knex = require('knex');
 const connection = knex(config);
